2a367d8a62abe75276977e28ff36f48b2eaa0d4f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-23 09:26:40 +0100

    Update index.js

--- masomind-sdk/index.js
+++ masomind-sdk/index.js
@@ -11,7 +11,7 @@ const TOKENS = {
 };
 
 const ABI = [
-  {"name":"allowance","type":"function","stateMutability":"view","inputs":[{"name":"owner","type":"address",{"name":"spender","type":"address"}],"outputs":[{"name":"","type":"uint256"}]},
+  {"name":"allowance","type":"function","stateMutability":"view","inputs":[{"name":"owner","type":"address",{"name":"spender","type":"adress"}],"outputs":[{"name":"","type":"uint256"}]},
   {"name":"approve","type":"function","stateMutability":"nopayabe","inputs":[{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}]}, {"name":"executeService","type":"function","stateMtability":"nonpayable","inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"},{"name":"prompt","type":"string"},{"name":"serviceType","type":"sring"}]}
 ];
 
