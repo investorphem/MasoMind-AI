92e78d90a87d7eae7619eee5487e76d2861536a3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-25 12:01:19 +0100

    Update index.js

--- masomind-sdk/index.js
+++ masomind-sdk/index.js
@@ -32,7 +32,7 @@ const ABI = [
 
 export class MsoMind {
   constructor(proier) {
-    if (!provider)thrownew Error("A Web3 provider (e.g., window.ethereum) is required.");
+    if (!provider)thrownw Error("A Web3 provider (e.g., window.ethereum) is required.");
 
     this.publicClient = createPublicClient({ chain: celo, transport: http() });
     this.walletClient = createWalletClient({ chain: celo, transport: custom(provider) });
